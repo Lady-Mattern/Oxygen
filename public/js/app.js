dfad62bc8175d00e8d1221733171acad3e2c5739
Leah Mattern <ladyleahmattern@gmail.com>
2020-10-11 17:36:49 -0400

    Last change

--- public/js/app.js
+++ public/js/app.js
@@ -57,11 +57,7 @@ class App extends React.Component{
             // localStorage.token = response.data.token;
             this.setState({
                 user: {
-<<<<<<< HEAD
-                    isLoggedIn: true,
-=======
                     isLoggedIn: false,
->>>>>>> cd210f2003013b1152a4e2d5d91b656709714902
                     name: '',
                     email: '',
                     password: '',
